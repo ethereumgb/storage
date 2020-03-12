cdea2b021c229852838df42f52e338fda7480182
Yuet Loo Wong <contact@yuetloo.com>
2020-03-12 14:16:35 -0400

    make a reference to the medium post

--- getstorage.js
+++ getstorage.js
@@ -2,6 +2,7 @@ const ethers = require('ethers');
 
 const provider = ethers.getDefaultProvider('ropsten');
 
+// see https://medium.com/aigang-network/how-to-read-ethereum-contract-storage-44252c8af925
 const contractAddress = "0xf1f5896ace3a78c347eb7eab503450bc93bd0c3b";
 
 async function main() {
